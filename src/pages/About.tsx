905615938bcf646bd12b55ef5ba1ccf131fdac00
Rohit Bagade <126180166+Rohitbagade-cs@users.noreply.github.com>
2025-03-02 02:03:18 +0530

    about us pic updated

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -44,7 +44,7 @@ const About = () => {
                 className="relative"
               >
                 <img
-                  src="/images/mission.jpg"
+                  src="https://images.unsplash.com/photo-1600880292203-757bb62b4baf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
                   alt="Our Mission"
                   className="rounded-2xl shadow-lg w-full h-auto object-cover"
                 />
